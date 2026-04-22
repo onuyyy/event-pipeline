2281bf09443f72f45d9c7852bc07e87694eb26e8
onuyyy <qazxsw9026@naver.com>
2026-04-23 06:39:04 +0900

    chore : Spring Boot 프로젝트 초기 파일 추가

---
+++ src/main/java/com/eventpipeline/EventPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.eventpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPipelineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventPipelineApplication.class, args);
+  }
+
+}
